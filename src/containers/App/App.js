f6c277c331a4ea8c650c0dbe746cfa3e3c0c08b0
jonraem <joni.ramo.91@gmail.com>
2017-10-26 12:55:22 +0300

    Add styling for Buttons and restructure App

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -1,16 +1,39 @@
 import React, { Component } from 'react';
 
 import Header from '../../components/Header/Header';
+import Buttons from '../../components/Buttons/Buttons';
 import './App.css';
 
 export class App extends Component {
+  constructor(props) {
+    super(props);
+  }
+  
+  componentWillMount() {
+    const script = document.createElement('script');
+
+    script.src = 'https://use.fontawesome.com/8d1def7579.js';
+    script.async = true;
+
+    document.body.appendChild(script);
+  }
+
   render() {
     return (
       <div className="App">
         <Header/>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="App-content">
+          <Buttons/>
+          <div className="App-intro">
+            <p>
+              Hello, my name is Joni Rämö. I'm a web developer and entrepreneur from the beautiful city of Turku in Finland.
+            </p>
+            <p>
+              Find out more about me or drop me an email to say hello!
+            </p>
+          </div>
+        </div>
+        <div className="App-footer"></div>
       </div>
     );
   }
