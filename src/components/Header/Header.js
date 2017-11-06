47812a46868e8274cf368aa146cab728c8f08f20
jonraem <joni.ramo.91@gmail.com>
2017-11-06 12:49:47 +0200

    Add signature logo and animate it

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,11 +1,18 @@
 import React, { Component } from 'react';
+import ReactSVG from 'react-svg';
 
+import signature from '../../assets/img/signature.svg';
 import './Header.css';
 
 export class Header extends Component {
   render() {
     return (
       <header className="App-header">
+        <ReactSVG
+          className="App-logo"
+          path={signature}
+          alt="Logo"
+        />
       </header>
     );
   }
