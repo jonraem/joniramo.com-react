a0e97a32e5ad46a4e82a0e49f623dbe5ce02b243
jonraem <joni.ramo.91@gmail.com>
2017-11-06 22:50:14 +0200

    Add Google Analytics

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import ReactGA from 'react-ga';
+
 import App from './containers/App/App';
 import registerServiceWorker from './utils/registerServiceWorker';
+import './index.css';
+
+ReactGA.initialize('insert-tracking-here');
+ReactGA.pageview(window.location.pathname + window.location.search);
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
