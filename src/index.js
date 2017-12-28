1fa1f4bd6e20ddffeab8c2d38be6ece84599d764
jonraem <joni.ramo.91@gmail.com>
2017-12-28 20:46:56 +0200

    Remove non-working local implementation of FontAwesome

--- src/index.js
+++ src/index.js
@@ -6,8 +6,6 @@ import App from './containers/App/App';
 import registerServiceWorker from './utils/registerServiceWorker';
 import './index.css';
 
-import './assets/js/fontawesome-all.min.js';
-
 ReactGA.initialize('insert-tracking-here');
 ReactGA.pageview(window.location.pathname + window.location.search);
 
