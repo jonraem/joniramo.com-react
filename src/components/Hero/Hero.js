de1ba87a4c611299114d435bb002a39e9a87275a
jonraem <joni.ramo.91@gmail.com>
2018-12-23 17:59:17 +0200

    Update Snowfall

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,22 +1,22 @@
 import React, { Component } from 'react';
 import moment from 'moment';
-import classnames from 'classnames';
 import ReactSVG from 'react-svg';
 
 import signature from '../../assets/img/signature.svg';
 import './Hero.css';
-import './ChristmasOverlay.css';
+import './Snowfall.css';
 
 export class Hero extends Component {
   render() {
-    let overlayClassName = 'overlay';
-    if (moment().month() === 11) {
-      overlayClassName = classnames(overlayClassName, 'snowfall__one', 'snowfall__two', 'snowfall__three');
-    }
+    const isChristmasTime = moment().month() === 11;
 
     return (
       <div className="App__hero">
-        <div className="App__hero-overlay" />
+        <div className="App__hero-overlay">
+          {isChristmasTime && (
+            <div className={'snowfall__one snowfall__two snowfall__three'} />
+          )}
+        </div>
         <div className="App__hero-logo">
           <ReactSVG path={signature} alt="Logo" />
         </div>
