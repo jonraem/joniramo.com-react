67df49d7a7c885df5db2e7f942f2d5f1a79c943e
jonraem <joni.ramo.91@gmail.com>
2018-12-23 18:20:40 +0200

    Remove react-ga

--- src/index.js
+++ src/index.js
@@ -1,13 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import ReactGA from 'react-ga';
 
 import App from './containers/App/App';
 import registerServiceWorker from './utils/registerServiceWorker';
 import './index.css';
-
-ReactGA.initialize('insert-tracking-here');
-ReactGA.pageview(window.location.pathname + window.location.search);
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
