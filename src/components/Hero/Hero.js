b963a1adf897295b5e882076f816e34378260394
jonraem <joni.ramo.91@gmail.com>
2018-12-23 19:02:13 +0200

    Add Christmas hero image

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import moment from 'moment';
+import classnames from 'classnames';
 import ReactSVG from 'react-svg';
 
 import Snowfall from '../Snowfall/Snowfall';
@@ -10,8 +11,13 @@ export class Hero extends Component {
   render() {
     const isChristmasTime = moment().month() === 11;
 
+    let heroClassName = 'App__hero';
+    if (isChristmasTime) {
+      heroClassName = classnames(heroClassName, 'App__hero--xmas');
+    }
+
     return (
-      <div className="App__hero">
+      <div className={heroClassName}>
         <div className="App__hero-overlay">
           {isChristmasTime && (
             <Snowfall />
