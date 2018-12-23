76d70c1c471e81507aed837c7fcc40c592585901
jonraem <joni.ramo.91@gmail.com>
2018-12-23 20:35:54 +0200

    Add app wide text color differently

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
 import App from './containers/App/App';
 import registerServiceWorker from './utils/registerServiceWorker';
 import './index.scss';
