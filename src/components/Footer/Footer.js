9fdc4ce6dc3302ddd14e0637dd2ce9db65e4a611
jonraem <joni.ramo.91@gmail.com>
2018-12-23 20:39:48 +0200

    Add attribution text to footer if it's Christmas

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { isChristmasTime } from '../../utils/helpers';
 import './Footer.scss';
 
 export class Footer extends Component {
@@ -8,6 +9,9 @@ export class Footer extends Component {
     return (
       <footer className="App__footer">
         <p>© Joni Rämö {today.getFullYear()}</p>
+        {isChristmasTime && (
+          <div className="App__footer-attribution">Christmas icons made by <a href="https://www.flaticon.com/authors/dimitry-miroliubov" title="Dimitry Miroliubov">Dimitry Miroliubov</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a>.</div>
+        )}
       </footer>
     );
   }
