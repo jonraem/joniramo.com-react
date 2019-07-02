3c59158f9c95fdab3c7775f81ac12bc13be7c6b1
Joni Rämö <joni.ramo.91@gmail.com>
2019-07-02 15:21:28 +0300

    Add optional image property for TileButton

--- src/components/TileButton/TileButton.js
+++ src/components/TileButton/TileButton.js
@@ -2,15 +2,31 @@ import React from 'react'
 import classnames from 'classnames';
 import './TileButton.scss';
 
-const TileButton = ({ className, link, label, ariaLabel, iconClassName }) => {
-  const buttonClassName = classnames('App__tile-button', 'animated-button', 'thar-three', className);
+const TileButton = (props) => {
+  const buttonClassName = classnames(
+    'App__tile-button',
+    'animated-button',
+    'thar-three',
+    props.className || ''
+  );
+
   return (
-    <a className={buttonClassName} href={link} rel="noopener noreferrer" aria-label={ariaLabel}>
-      <span className={!!iconClassName && 'App__tile-button-label'}>
-        {label}
+    <a className={buttonClassName} href={props.link} rel="noopener noreferrer" aria-label={props.ariaLabel}>
+      <span className={props.iconClassName ? 'App__tile-button-label' : undefined}>
+        {props.label}
       </span>
-      {!!iconClassName && (
-        <i className={iconClassName} aria-hidden="true"></i>
+      {!!props.iconClassName && (
+        <i
+          className={classnames('App__tile-icon', props.iconClassName)}
+          aria-hidden="true"
+        />
+      )}
+      {!!props.imageSrc && (
+        <img
+          className={classnames('App__tile-image', props.imageClassName)}
+          src={props.imageSrc}
+          alt={props.ariaLabel}
+        />
       )}
     </a>
   );
