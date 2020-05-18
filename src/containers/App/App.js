38682ce8923ba1adb237be14ea9225a5a452c408
Joni Rämö <joni.ramo.91@gmail.com>
2020-05-18 13:34:29 +0300

    Remove a couple taglines

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -1,44 +1,44 @@
-import React, { Component } from 'react';
-import Hero from '../../components/Hero/Hero';
-import Buttons from '../../components/Buttons/Buttons';
-import Footer from '../../components/Footer/Footer';
-import './App.scss';
+import React, { Component } from "react";
+import Hero from "../../components/Hero/Hero";
+import Buttons from "../../components/Buttons/Buttons";
+import Footer from "../../components/Footer/Footer";
+import "./App.scss";
 
 const getRandomTagline = () => {
   const listOfTaglines = [
-    'keep it fresh',
-    'stay awesome',
-    'strive for greatness',
-    'work smart, not hard',
-    'believe in yourself',
-    'remember to smile',
-    'just do it',
-    'keep hustling',
-    'keep on trucking\'',
-    'rock on',
-    'eat your vegetables',
-    'stay hydrated',
-    'exercise regularly',
-    'don\'t drink and drive',
-    'always wear a helmet',
-    'live long and prosper',
-    'may the Force be with you',
-    'always look on the bright side of life'
+    "keep it fresh",
+    "stay awesome",
+    "strive for greatness",
+    "believe in yourself",
+    "remember to smile",
+    "keep hustling",
+    "keep on trucking'",
+    "rock on",
+    "eat your vegetables",
+    "stay hydrated",
+    "exercise regularly",
+    "don't drink and drive",
+    "always wear a helmet",
+    "live long and prosper",
+    "may the Force be with you",
+    "always look on the bright side of life",
   ];
   const max = listOfTaglines.length;
   const randomIndex = Math.floor(Math.random() * Math.floor(max));
-  
-  return <span className="App__intro-tagline">{listOfTaglines[randomIndex]}</span>;
+
+  return (
+    <span className="App__intro-tagline">{listOfTaglines[randomIndex]}</span>
+  );
 };
 
 export class App extends Component {
   componentWillMount() {
-    const link = document.createElement('link');
-    link.rel = 'stylesheet';
-    link.href = 'https://fonts.googleapis.com/css?family=Roboto+Mono';
+    const link = document.createElement("link");
+    link.rel = "stylesheet";
+    link.href = "https://fonts.googleapis.com/css?family=Roboto+Mono";
 
-    const script = document.createElement('script');
-    script.src = 'https://use.fontawesome.com/releases/v5.0.2/js/all.js';
+    const script = document.createElement("script");
+    script.src = "https://use.fontawesome.com/releases/v5.0.2/js/all.js";
     script.defer = true;
 
     document.head.appendChild(link);
@@ -53,14 +53,16 @@ export class App extends Component {
           <Buttons />
           <div className="App__intro">
             <p>
-              Hello, my name is Joni. I'm a <u>web developer</u> currently residing in Helsinki, the beautiful capital of Finland. I love practical design, solving creative challenges and meeting new people.
+              Hello, my name is Joni. I'm a <u>web developer</u> currently
+              residing in Helsinki, the beautiful capital of Finland. I love
+              practical design, solving creative challenges and meeting new
+              people.
             </p>
             <p>
-              Find out more about me, and get in touch with me for any inquiries or simply to say hi.
+              Find out more about me, and get in touch with me for any inquiries
+              or simply to say hi.
             </p>
-            <p>
-              And {getRandomTagline()}.
-            </p>
+            <p>And {getRandomTagline()}.</p>
           </div>
         </div>
         <Footer />
