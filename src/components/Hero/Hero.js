a3481808aa7849338c2a425bb25a76490cf14194
Joni Rämö <joni.ramo.91@gmail.com>
2020-05-18 13:29:08 +0300

    Fix import for ReactSVG

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,30 +1,24 @@
-import React, { Component } from 'react';
-import classnames from 'classnames';
-import ReactSVG from 'react-svg';
-import Christmas from '../Christmas/Christmas';
-import { isChristmasTime } from '../../utils/helpers';
-import signature from '../../assets/svg/signature.svg';
-import './Hero.scss';
+import React, { Component } from "react";
+import classnames from "classnames";
+import { ReactSVG } from "react-svg";
+import Christmas from "../Christmas/Christmas";
+import { isChristmasTime } from "../../utils/helpers";
+import signature from "../../assets/svg/signature.svg";
+import "./Hero.scss";
 
 export class Hero extends Component {
   render() {
-    let heroClassName = 'App__hero';
+    let heroClassName = "App__hero";
     if (isChristmasTime) {
-      heroClassName = classnames(heroClassName, 'App__hero--xmas');
+      heroClassName = classnames(heroClassName, "App__hero--xmas");
     }
 
     return (
       <div className={heroClassName}>
         <div className="App__hero-overlay">
-          {isChristmasTime && (
-            <Christmas />
-          )}
+          {isChristmasTime && <Christmas />}
         </div>
-        <ReactSVG
-          src={signature}
-          className="App__hero-logo"
-          alt="Joni Rämö"
-        />
+        <ReactSVG src={signature} className="App__hero-logo" alt="Joni Rämö" />
       </div>
     );
   }
